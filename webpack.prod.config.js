295d9bf9756c37a3aadda3dcd5f3dd4a8325c974
Haomin Wu <wuhaominsysu@gmail.com>
2017-10-14 11:12:03 -0400

    Add production webpack config and build command

---
+++ webpack.prod.config.js
@@ -0,0 +1,46 @@
+var webpack = require('webpack');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: `${__dirname}/build`,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015', 'stage-1']
+      },
+    },
+    {
+      test: /\.css$/,
+      loader: 'style-loader!css-loader'
+    }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+      },
+    }),
+    new CopyWebpackPlugin(
+    [{
+      from: './*.html',
+      to: `${__dirname}/build`
+    },
+    {
+      from: './style',
+      to: `${__dirname}/build/style`
+    }])
+  ]
+};
