83a17eac5cbd4880acc3705e11b1f7e06691fdfa
Haomin Wu <wuhaominsysu@gmail.com>
2017-10-14 22:39:06 -0400

    Change openweather http request to https

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -10,27 +10,29 @@ GoogleMapsLoader.KEY = require('../config/keys').GOOGLE_MAP_API_KEY;
 
 class WeatherList extends Component{
   renderWeather(cityData){
-    const name = cityData.city.name;
-    const temps = cityData.list.map( weather => weather.main.temp);
-    const pressures = cityData.list.map( weather => weather.main.pressure);
-    const humidities = cityData.list.map( weather => weather.main.humidity);
-    const lon = cityData.city.coord.lon;
-    const lat = cityData.city.coord.lat;
+    if (cityData != null) {
+      const name = cityData.city.name;
+      const temps = cityData.list.map( weather => weather.main.temp);
+      const pressures = cityData.list.map( weather => weather.main.pressure);
+      const humidities = cityData.list.map( weather => weather.main.humidity);
+      const lon = cityData.city.coord.lon;
+      const lat = cityData.city.coord.lat;
 
-    return (
-      <tr key={ name }>
-        <td><GoogleMap lon={lon} lat={lat} /></td>
-        <td>
-          <Chart data={temps} color = "orange" units="K"/>
-        </td>
-        <td>
-          <Chart data={pressures} color = "green" units="hPa"/>
-        </td>
-        <td>
-          <Chart data={humidities} color = "black" units="%"/>
-        </td>
-      </tr>
-    );
+      return (
+        <tr key={ name }>
+          <td><GoogleMap lon={lon} lat={lat} /></td>
+          <td>
+            <Chart data={temps} color = "orange" units="K"/>
+          </td>
+          <td>
+            <Chart data={pressures} color = "green" units="hPa"/>
+          </td>
+          <td>
+            <Chart data={humidities} color = "black" units="%"/>
+          </td>
+        </tr>
+      );
+    }
   }
 
   render(){
