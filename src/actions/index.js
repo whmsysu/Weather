82b4c9afbee7a26e7cc8de6d1204608ac1b32497
Haomin Wu <wuhaominsysu@gmail.com>
2017-10-14 22:40:02 -0400

    Change openweather api to https

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${ require('../config/keys').OPEN_WEATHER_MAP_API_KEY }`;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${ require('../config/keys').OPEN_WEATHER_MAP_API_KEY }`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
