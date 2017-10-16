7771b236046b0ae5bef933a1e2738137f9f29807
Haomin Wu <wuhaominsysu@gmail.com>
2017-10-16 17:06:06 -0400

    Remove us from api call

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,7 +5,7 @@ const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${ requ
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city){
-  const url = `${ROOT_URL}&q=${city},us`;
+  const url = `${ROOT_URL}&q=${city}`;
   const request = axios.get(url);
   return {
     type: FETCH_WEATHER,
